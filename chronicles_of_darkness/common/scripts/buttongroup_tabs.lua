740d562a65df9cbc9a20f029f532dbda78a24115
James T. Boylan <drahkar@good-bad-ugly.net>
2016-05-03 05:58:05 -0500

    Resolved issues with tabcontrol.

--- chronicles_of_darkness/common/scripts/buttongroup_tabs.lua
+++ chronicles_of_darkness/common/scripts/buttongroup_tabs.lua
@@ -1,181 +1,128 @@
--- 
--- Please see the license.html file included with this distribution for 
+-- Please see the license.html file included with this distribution for
 -- attribution and copyright information.
---
--- TODO: Integrate this code into the tabcontrol code so that ruleset can dynamically control charsheet tabs
+
+-- TODO: Integrate this code into the buttongroup_tabs code so that ruleset can dynamically control charsheet tabs
 local topWidget = nil;
 local tabIndex = 1;
+local tabWidgets = {};
+local tabs = {};
 
-local aTabs = {};
-
-local aTabOffset = { 7, 41 };
-local aHelperOffset = { 8, 7 };
-local nTabHeight = 67;
-local nMargins = 25;
-
-function onInit()
-	-- Create a helper graphic widget to indicate that the selected tab is on top
-	topWidget = addBitmapWidget("tabtop");
-	topWidget.setVisible(false);
-
-	-- Deactivate all labels
-	if tab and type(tab) == "table" then
-		for n, v in ipairs(tab) do
-			if type(v) == "table" then
-				setTab(n, v.subwindow[1], v.icon[1]);
-			end
-		end
+function addTab(subwindow,icon,scroller)
+	local index = #tabs + 1;
+	local newtab = {};
+	newtab.subwindow = subwindow;
+	newtab.icon = icon;
+	newtab.scroller = scroller;
+	tabs[index] = newtab;
+	if icon then
+		tabWidgets[index] = addBitmapWidget(icon);
 	end
-
-	if activate then
-		activateTab(activate[1]);
-	else
-		activateTab(1);
-	end
-end
-
-function hideControls(index)
-	if aTabs[index] then
-		for _,v in ipairs(aTabs[index].controls) do
-			window[v].setVisible(false);
-			if window[v].onTabVisChanged then
-				window[v].onTabVisChanged(false);
-			end
-		end
-	end
-end
-
-function showControls(index)
-	if aTabs[index] then
-		for _,v in ipairs(aTabs[index].controls) do
-			window[v].setVisible(true);
-			if window[v].onTabVisChanged then
-				window[v].onTabVisChanged(true);
-			end
-		end
-	end
+	update();
 end
 
 function activateTab(index)
-	if tabIndex == index then
+	local thistab;
+	index = tonumber(index);
+	if index<1 or index>#tabs then
 		return;
 	end
-	
-	if tabIndex >= 1 and tabIndex <= #aTabs then
-		-- Hide active tab, fade text labels
-		aTabs[tabIndex].widget.setColor("80ffffff");
-		hideControls(tabIndex);
+
+	-- Hide active tab, fade text labels
+	thistab = tabs[tabIndex]
+	if tabWidgets[tabIndex] then
+		tabWidgets[tabIndex].setColor("80ffffff");
+	end
+	if window[thistab.subwindow] then
+		window[thistab.subwindow].setVisible(false);
+	end
+	if thistab.scroller then
+		window[thistab.scroller].setVisible(false);
 	end
 
 	-- Set new index
-	tabIndex = tonumber(index);
+	tabIndex = index;
+	thistab = tabs[tabIndex];
 
 	-- Move helper graphic into position
-	topWidget.setPosition("topleft", aHelperOffset[1], (nTabHeight * (tabIndex - 1)) + aHelperOffset[2]);
+	topWidget.setPosition("topleft", 5, 67*(tabIndex-1)+7);
 	if tabIndex == 1 then
 		topWidget.setVisible(false);
 	else
 		topWidget.setVisible(true);
 	end
-		
-	if tabIndex >= 1 and tabIndex <= #aTabs then
-		-- Activate text label and subwindow
-		aTabs[tabIndex].widget.setColor("ffffffff");
-		showControls(tabIndex);
+
+	-- Activate text label and subwindow
+	if tabWidgets[tabIndex] then
+		tabWidgets[tabIndex].setColor("ffffffff");
+	end
+	if window[thistab.subwindow] then
+		window[thistab.subwindow].setVisible(true);
+	end
+	if thistab.scroller then
+		window[thistab.scroller].setVisible(true);
 	end
 
-	if self.onTabChanged then
-		self.onTabChanged(tabIndex);
+	-- Call the onActivate handler, if present
+	if self.onActivate then
+		self.onActivate(index);
 	end
-end
 
-function setTab(index, sSub, sGraphic)
-	if aTabs[index] then
-		if sSub and sGraphic then
-			if sSub == aTabs[index].sub and sGraphic == aTabs[index].icon then
-				return;
-			end
-			if index == tabIndex then
-				hideControls(tabIndex);
-				aTabs[index].sub = sSub;
-				aTabs[index].controls = StringManager.split(sSub, ",", true);
-			else
-				aTabs[index].sub = sSub;
-				aTabs[index].controls = StringManager.split(sSub, ",", true);
-			end
-			
-			aTabs[index].widget.destroy();
-			aTabs[index].icon = sGraphic;
-			aTabs[index].widget = addBitmapWidget(sGraphic);
-			aTabs[index].widget.setPosition("topleft", aTabOffset[1], (nTabHeight * (index - 1)) + aTabOffset[2]);
-
-			if index == tabIndex then
-				aTabs[index].widget.setColor("ffffffff");
-				showControls(tabIndex);
-			else
-				aTabs[index].widget.setColor("80ffffff");
-			end
-		else
-			if index == tabIndex then
-				hideControls(index);
-			end
-			aTabs[index].widget.destroy();
-			aTabs[index] = nil;
-		end
-	else
-		if sSub and sGraphic then
-			local rTab = {};
-			
-			rTab.sub = sSub;
-			rTab.controls = StringManager.split(sSub, ",", true);
-			
-			rTab.icon = sGraphic;
-			rTab.widget = addBitmapWidget(sGraphic);
-			rTab.widget.setPosition("topleft", aTabOffset[1], (nTabHeight * (index - 1)) + aTabOffset[2]);
-			if index == tabIndex then
-				rTab.widget.setColor("ffffffff");
-			else
-				rTab.widget.setColor("80ffffff");
-			end
-			
-			aTabs[index] = rTab;
-
-			if index == tabIndex then
-				showControls(tabIndex);
-			end
-		end
-	end
-	
-	local nMax = 0;
-	for k,_ in pairs(aTabs) do
-		nMax = math.max(k, nMax);
-	end
-	setAnchoredHeight(nMargins + (nTabHeight * nMax));
-	
-	if tabIndex > nMax then
-		activateTab(nMax);
-	end
-end
-
-function getIndex()
-	return tabIndex;
+	-- Done
 end
 
 function onClickDown(button, x, y)
-	return true;
-end
+	local i = math.ceil(y/67);
 
-function onClickRelease(button, x, y)
-	local i = math.ceil(y / nTabHeight);
-
-	if i >= 1 and i <= #aTabs then
+	-- Make sure index is in range and activate selected
+	if i > 0 and i < #tabs+1 then
 		activateTab(i);
 	end
-	
-	return true;
 end
 
 function onDoubleClick(x, y)
-	-- Emulate click
-	onClickRelease(1, x, y);
+	-- Emulate single click
+	onClickDown(1, x, y);
+end
+
+function update()
+	-- set the control height
+	local h = #tabs * 67 + 22;
+	setAnchoredHeight(h);
+	-- position the tab icons
+	for n, v in ipairs(tabs) do
+		if tabWidgets[n] then
+			tabWidgets[n].setPosition("topleft", 7, 67*(n-1)+41);
+			tabWidgets[n].setColor("80ffffff");
+		end
+	end
+end
+
+function onInit()
+	-- Create a helper graphic widget to indicate that the selected tab is on top
+	topWidget = addBitmapWidget("tabtop");
+	topWidget.setVisible(false);
+	-- add all the static tabs
+	if tab and tab[1] then
+		for n, v in ipairs(tab) do
+			if type(v)~="boolean" then
+				local subwindow,icon,scroller;
+				if v.subwindow and v.subwindow[1] then
+					subwindow = v.subwindow[1];
+				end
+				if v.icon and v.icon[1] then
+					icon = v.icon[1];
+				end
+				if v.scroller and v.scroller[1] then
+					scroller = v.scroller[1];
+				end
+				addTab(subwindow,icon,scroller);
+			end
+		end
+	end
+	-- refresh the layout
+	update();
+	-- set the active tab, if there is one
+	if activate then
+		activateTab(activate[1]);
+	end
 end
